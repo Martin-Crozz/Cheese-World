6a1229aed041375126766b5242e406fe9b352d78
Martin-Crozz <martinmystery39@gmail.com>
2021-12-14 15:51:13 +0545

    Changes

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -9,17 +9,6 @@ var preloader = document.getElementById("loading");
 function myFunction() {
     preloader.style.display = 'none';
 };
-//Menu On Hover
-$('body').on('mouseenter mouseleave', '.nav-item', function(e) {
-    if ($(window).width() > 750) {
-        var _d = $(e.target).closest('.nav-item');
-        _d.addClass('show');
-        setTimeout(function() {
-            _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
-        }, 1);
-    }
-});
-// End Menu On Hover
 // Banner Slick Slider Starts
 $('.slick-slider').slick({
     infinite: true,
@@ -66,67 +55,55 @@ $('.slick-slider').slick({
 });
 // Banner Slick Slider Ends
 // Navigation Js Scroll Starts
+$(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 150) {
+            $(".navigation-wrap").css("background", "white");
+            $(".nav-link").css("color", "#484d67");
+            $(".navigation-wrap").css("box-shadow", "rgb(210 210 210) 0px 0px 6px 0px");
+            $(".navigation-wrap").css("padding", "10px 0px");
+        } else {
+            $(".navigation-wrap").css("background", "white");
+            $(".nav-link").css("color", "#484d67");
+            $(".navigation-wrap").css("box-shadow", "none");
+            $(".navigation-wrap").css("padding", "20px 0px");
+        }
+    }) // Navigation Js Scroll Ends
 $(document).ready(function() {
-    $(window).scroll(function() {
-            var scroll = $(window).scrollTop();
-            if (scroll > 150) {
-                $(".navigation-wrap").css("background", "white");
-                $(".nav-link").css("color", "#484d67");
-                $(".navigation-wrap").css("box-shadow", "rgb(210 210 210) 0px 0px 6px 0px");
-                $(".navigation-wrap").css("padding", "10px 0px");
-            } else {
-                $(".navigation-wrap").css("background", "white");
-                $(".nav-link").css("color", "#484d67");
-                $(".navigation-wrap").css("box-shadow", "none");
-                $(".navigation-wrap").css("padding", "20px 0px");
-            }
-        })
-        // Dropdown Hover Open
-    $('.nav-item').on('mouseover', function() {
-        $(this).find('.dropdown-menu').addClass('show');
-    })
-    $('.nav-item').on('mouseout', function() {
-        $(this).find('.dropdown-menu').removeClass('show');
-    })
-});
-// Navigation Js Scroll Ends
-
-// product Gallery and Zoom
-
-// activation carousel plugin
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 5,
-    freeMode: true,
-
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    breakpoints: {
-        0: {
-            slidesPerView: 3,
+    // product Gallery and Zoom
+    // activation carousel plugin
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 5,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 3,
+            },
+            992: {
+                slidesPerView: 4,
+            },
+        }
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        992: {
-            slidesPerView: 4,
+        thumbs: {
+            swiper: galleryThumbs
         },
-    }
+    });
+    // change carousel item height
+    // gallery-top
+    let productCarouselTopWidth = $('.gallery-top').outerWidth();
+    $('.gallery-top').css('height', productCarouselTopWidth);
+    // gallery-thumbs
+    let productCarouselThumbsItemWith = $('.gallery-thumbs .swiper-slide').outerWidth();
+    $('.gallery-thumbs').css('height', productCarouselThumbsItemWith);
+    // activation zoom plugin
+    // var $easyzoom = $('.easyzoom').easyZoom();
+    // Dropdown Menu On Hover
 });
-var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    thumbs: {
-        swiper: galleryThumbs
-    },
-});
-// change carousel item height
-// gallery-top
-let productCarouselTopWidth = $('.gallery-top').outerWidth();
-$('.gallery-top').css('height', productCarouselTopWidth);
-
-// gallery-thumbs
-let productCarouselThumbsItemWith = $('.gallery-thumbs .swiper-slide').outerWidth();
-$('.gallery-thumbs').css('height', productCarouselThumbsItemWith);
-
-// activation zoom plugin
-// var $easyzoom = $('.easyzoom').easyZoom();
